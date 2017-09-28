8e0edee127ace41f7d5043c993cb9100e0e34aa9
Maren Sigson <msigson@gmail.com>
2017-09-28 14:48:22 -0400

    debugged mobile menu issue

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,13 +2,7 @@ $(document).ready(function(){
 
 	$('.landing__hamburgerMenu').click(function(){
 		$('#nav-icon').toggleClass('open');
-		// $('ul').addClass('flex');
 
-		// if ($('#nav-icon').hasClass('open')){
-		// 	$('ul').addClass('openNav');
-		// } else {
-		// 	$('ul').removeClass('openNav');
-		// }
 	});
 
 	$("a").on('click', function(e) {
@@ -22,24 +16,8 @@ $(document).ready(function(){
 			}, 600, function(){});
 		} 
 
+		document.getElementById("toggle").checked = false;
+
+		$('#nav-icon').toggleClass('open');
 	});
-
-	// $(window).on('resize', function(){
-	// 	const win = $(this);
-	// 	if (win.width() < 651) {
-	// 		$('#nav-icon').removeClass('open');
-	// 		$('ul').removeClass('openNav');
-			
-	// 	} if (win.width() > 650) {
-	// 		$('ul').removeClass('flex');
-	// 		$('ul').addClass('openNav');
-	// 	} 
-	// });
-
-
 });
-
-
-// if ($(window).width() > 650 ){
-// 	$('ul').addClass('openNav');
-// }
